286110de2ef40c6a37968da9ef426318803cce8d
TechnologyHell <nitishsinghslg@gmail.com>
2024-07-07 21:24:15 +0530

    day1

---
+++ 0_basics/calculator.py
@@ -0,0 +1,10 @@
+a = int(input("Enter 1st number : "))
+b = int(input("Enter 2nd number : "))
+
+print("Addition = ", (a+b))
+print("Subtraction = ", (a-b))
+print("Miltiplication = ", (a*b))
+print("Division = ", (a/b))
+print("Modulus = ", (a%b))
+print("Exponent = ", (a**b))
+print("Floor Division = ", (a//b))
